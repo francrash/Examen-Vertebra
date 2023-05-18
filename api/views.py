5a79fd50580aedd6123b89070c5e260457075370
fchacon <franfc2.pri@gmail.com>
2023-05-18 11:44:59 +0200

    Serializers

--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from api import serializers
 # Create your views here.
+
+from .models import Job, Images
+from .serializers import JobSerializer, ImagenSerializer
+
+
+@api_view(['POST'])
+def post_image(request):
+    data = request.data
+    images = Images.objects.create(
+
+    )
